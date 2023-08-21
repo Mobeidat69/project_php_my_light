bb74596697a3596d9d70f0a8ed7ff03dd1d43f84
somaiah <somiayaseen59@gmail.com>
2023-08-21 21:16:02 +0300

    delete all

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-header('Location: index.php'); // Redirect to the landing page or desired location after log out
-exit();
-?>
